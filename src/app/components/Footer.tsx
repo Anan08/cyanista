46e1391a735bc82998702a55ca6080ec9514cec8
Ananda Putra Andika <anandapgt@gmail.com>
2023-12-02 18:16:30 +0700

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div className='bg-cyan-950 text-center py-4'>
+        <p className='font-bold'>Copyright 2023 CodeSakti. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
